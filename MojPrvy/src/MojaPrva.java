3e4b6a4cc91ccb1874704aefac2a5ec319df030a
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-10-19 12:16:20 +0200

    prvy program

---
+++ MojPrvy/src/MojaPrva.java
@@ -0,0 +1,31 @@
+
+public class MojaPrva {
+
+	public static void main(String[] args) {
+		
+		//jednoriadkovy komentar
+		
+		/*
+		 * toto je 
+		 * viacriadkovy 
+		 * komentar
+		 */
+		
+		/**
+		 * 
+		 */
+		System.out.println("ahoj");
+		System.out.println("4b");
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
