32342701991a53cddfeb72b9c2f8cadb93c27e78
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-11-24 08:04:06 +0100

    Update UBMI.java

--- MojPrvy/src/UBMI.java
+++ MojPrvy/src/UBMI.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 
 public class UBMI {
 
 	public static void main(String[] args) {
 
-		int hmotnost = 86;
-		int vyska = 186;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Zadaj hmotnost[kg]: ");
+		int hmotnost = sc.nextInt();
+		System.out.println("Zadaj vysku[cm]: ");
+		int vyska = sc.nextInt();
+		
+		double v = vyska / 100.0;
+		double bmi = hmotnost / (v * v);
+		System.out.println("BMI: " + bmi);
 		
 
 	}
