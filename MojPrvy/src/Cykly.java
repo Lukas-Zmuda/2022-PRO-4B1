ad088567585999154a7172229941fe638263e1c2
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2023-02-01 08:41:55 +0100

    Create Cykly.java

---
+++ MojPrvy/src/Cykly.java
@@ -0,0 +1,20 @@
+
+public class Cykly {
+
+	public static void main(String[] args) {
+		
+		//cyklus s pevnym poctom opakovani
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println((i + 1) + ". Fero");
+		}
+		
+		for(int i = 0; i < 11; i = i + 2) {
+			System.out.print(i + " ");
+		}
+		
+		System.out.println();
+
+	}
+
+}
